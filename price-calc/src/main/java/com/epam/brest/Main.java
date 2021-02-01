061d0d8cb8c10bb06c63ba225d30c602233f6ac3
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2021-02-01 21:12:01 +0300

    Fix previouse commit

--- price-calc/src/main/java/com/epam/brest/Main.java
+++ price-calc/src/main/java/com/epam/brest/Main.java
@@ -1,2 +1,21 @@
-package com.epam.brest;public class Main {
+package com.epam.brest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Distance
+        // Price per km
+        // Weight
+        // Price per kg
+        // Result = d * pr1 + w * pr2
+
+        double d = -10;
+        double pr1 = 20.05;
+        double w = 30;
+        double pr2 = 30.5;
+
+        double result = d*pr1+w*pr2;
+        System.out.println("Result: " + result);
+    }
+
 }
